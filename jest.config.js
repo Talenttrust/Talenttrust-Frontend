6bb8de7e4369d9705e1429c1042069352ff44b98
shaiananvari8 <228813044+shaiananvari8@users.noreply.github.com>
2026-06-19 19:27:00 -0500

    Fix CI audit after Next upgrade

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@
 const config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  modulePathIgnorePatterns: ['<rootDir>/.next/'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './.babelrc' }],
