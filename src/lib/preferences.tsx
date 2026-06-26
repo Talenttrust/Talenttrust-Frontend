3f4b53b230d2bf708f0bf63fef855547b5d49c13
muhsar27 <muhsar4321@gmail.com>
2026-06-26 22:54:16 +0100

    fix:lint tests

--- src/lib/preferences.tsx
+++ src/lib/preferences.tsx
@@ -23,7 +23,7 @@ function safeCurrencyFormat(
       style: 'currency',
       currency,
     }).format(amount);
-  } catch (e) {
+  } catch (_e) {
     return new Intl.NumberFormat(options.locale || 'en-US', {
       ...options,
       style: 'currency',
